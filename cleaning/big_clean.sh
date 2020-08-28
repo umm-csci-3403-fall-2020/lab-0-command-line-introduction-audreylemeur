3d3ef0a89ce58f2841a3da64f5030c23160c6907
Audrey <lemeu001@umn.edu>
2020-08-28 10:41:33 -0500

    Reorganize file structure

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -3,21 +3,22 @@
 here=$(pwd)
 
 SCRATCH=$(mktemp -d)
+FILEBASE=$(basename -s .tgz "$1")
 
 tar -xf "$1" -C "$SCRATCH"/
 
-grep -rlz "DELETE ME!" "$SCRATCH" | xargs rm -rf
+grep -rlz "DELETE ME!" "$SCRATCH"/"$FILEBASE" | xargs rm -rf
 
 cd "$SCRATCH" || exit
 
 for f in ./*/*
 do
         BASE=$(basename "$f")
-        mv "$f" cleaned_"$BASE"
-	echo "cleaned_$BASE"
+        mv "$f" "$FILEBASE"/cleaned_"$BASE"
+
 done
 
-tar -czf cleaned_"$1" "$SCRATCH"/*
+tar -czf cleaned_"$1" "$SCRATCH"/"$FILEBASE"
 mv cleaned_"$1" "$here"
 
 rm -r "$SCRATCH"
